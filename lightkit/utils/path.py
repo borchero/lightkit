3c86ebc674345122101c5b6239ed20f54d595e88
Oliver Borchert <me@borchero.com>
2022-12-12 01:58:33 +0100

    Refactor CI setup and add pre-commit configuration (#2)

--- lightkit/utils/path.py
+++ lightkit/utils/path.py
@@ -6,4 +6,4 @@ if sys.version_info < (3, 9, 0):
     # PathLike is not generic for Python 3.9
     PathType = Union[str, PathLike]
 else:
-    PathType = Union[str, PathLike[str]]
+    PathType = Union[str, PathLike[str]]  # type: ignore
